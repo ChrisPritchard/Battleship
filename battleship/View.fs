a869d32fef9e53dbfba90d1c5ae9662cc1b99321
Chris Pritchard <chris.pritchard@edpay.nz>
2018-05-23 15:02:46 +1200

    Added targetting cursor

--- battleship/View.fs
+++ battleship/View.fs
@@ -10,6 +10,7 @@ let resolution = Windowed ((offsetx*2)+(tilew*boardx),(offsety*2)+(tileh*boardy)
 
 let assetsToLoad = [
     Texture { key = "blank"; path = "Content/white" }
+    Texture { key = "cursor"; path = "Content/cursor" }
     Font { key = "default"; path = "Content/miramo" }
 ]
 
@@ -25,9 +26,11 @@ let getView runState model =
                     ColouredImage (Color.Gray, { assetKey = "blank"; destRect = rect; sourceRect = None })
                     ColouredText (Color.White, { assetKey = "default"; text = label; position = pos; origin = Centre; scale = 0.2 })
                 ]))
+    let (mx,my) = runState.mouse.position
 
     [ColouredImage (Color.Black, { assetKey = "blank"; destRect = offsetx,offsety,tilew*boardx,tileh*boardy; sourceRect = None })]
     @ ([0..boardx-1] |> List.collect (fun x -> [0..boardy-1] |> List.map (fun y -> 
         let rect = offsetx+x*tilew+1, offsety+y*tileh+1,tilew-2,tileh-2
         ColouredImage (Color.Blue, { assetKey = "blank"; destRect = rect; sourceRect = None }))))
     @ shipTiles
+    @ [Image { assetKey = "cursor"; destRect = mx-24,my-24,48,48; sourceRect = None }]
