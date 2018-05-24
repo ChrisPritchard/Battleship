b93b32465f6d56fccdaa66863d1db83883363531
Chris Pritchard <chris.pritchard@edpay.nz>
2018-05-25 09:38:20 +1200

    Cleaned up view, breaking board render functions into their own methods

--- battleship/View.fs
+++ battleship/View.fs
@@ -14,23 +14,30 @@ let assetsToLoad = [
     Font { key = "default"; path = "Content/miramo" }
 ]
 
+let boardTiles = 
+    let boardSize = offsetx, offsety, tilew*boardx, tileh*boardy
+    let back = ColouredImage (Color.Black, { assetKey = "blank"; destRect = boardSize; sourceRect = None })
+    let sea = 
+        [0..boardx-1] |> List.collect (fun x -> [0..boardy-1] |> List.map (fun y -> 
+            let rect = offsetx+x*tilew+1, offsety+y*tileh+1, tilew-2, tileh-2
+            ColouredImage (Color.Blue, { assetKey = "blank"; destRect = rect; sourceRect = None })))
+    back::sea
+
+let shipTiles (shipName,tiles) = 
+    let label = Seq.head shipName |> string
+    tiles |> List.collect(fun t -> 
+        let pos = offsetx+t.x*tilew+3+tilew/2,offsety+t.y*tileh+3+tileh/2
+        let rect = offsetx+t.x*tilew+3,offsety+t.y*tileh+3,tilew-6,tileh-6
+        [
+            ColouredImage (Color.Gray, { assetKey = "blank"; destRect = rect; sourceRect = None })
+            ColouredText (Color.White, { assetKey = "default"; text = label; position = pos; origin = Centre; scale = 0.2 })
+        ])
+
 let getView runState model = 
     let currentPlayer = if model.isFirstPlayerTurn then fst model.players else snd model.players
-    let shipTiles = 
-        currentPlayer.ships |> List.collect (fun (name,tiles) ->
-            let label = Seq.head name |> string
-            tiles |> List.collect(fun t -> 
-                let pos = offsetx+t.x*tilew+3+tilew/2,offsety+t.y*tileh+3+tileh/2
-                let rect = offsetx+t.x*tilew+3,offsety+t.y*tileh+3,tilew-6,tileh-6
-                [
-                    ColouredImage (Color.Gray, { assetKey = "blank"; destRect = rect; sourceRect = None })
-                    ColouredText (Color.White, { assetKey = "default"; text = label; position = pos; origin = Centre; scale = 0.2 })
-                ]))
+    let shipTiles = currentPlayer.ships |> List.collect shipTiles
+            
     let (mx,my) = runState.mouse.position
+    let cursor = Image { assetKey = "cursor"; destRect = mx-16,my-16,32,32; sourceRect = None }
 
-    [ColouredImage (Color.Black, { assetKey = "blank"; destRect = offsetx,offsety,tilew*boardx,tileh*boardy; sourceRect = None })]
-    @ ([0..boardx-1] |> List.collect (fun x -> [0..boardy-1] |> List.map (fun y -> 
-        let rect = offsetx+x*tilew+1, offsety+y*tileh+1,tilew-2,tileh-2
-        ColouredImage (Color.Blue, { assetKey = "blank"; destRect = rect; sourceRect = None }))))
-    @ shipTiles
-    @ [Image { assetKey = "cursor"; destRect = mx-16,my-16,32,32; sourceRect = None }]
+    boardTiles @ shipTiles @ [cursor]
